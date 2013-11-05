68deb1fbed426ad4b2f13a9582e0974e15d96087
Piedone <piedone@pyrocenter.hu>
2013-11-05 01:11:29 +0100

    Clean-up

--- QuickParts/IQuickPartLogic.cs
+++ QuickParts/IQuickPartLogic.cs
@@ -10,7 +10,6 @@ namespace Lombiq.Abstractions.QuickParts
     public interface IQuickPartLogic<T> : IQuickPartLogic
         where T : IQuickPart
     {
-        // TODO: How will generic methods be called? What about using the event bus (no real type information available then!)?
         IEnumerable<KeyValuePair<string, object>> ComputeDisplayParameters(T part);
     }
 }
