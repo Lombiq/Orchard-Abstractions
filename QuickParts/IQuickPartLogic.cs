301e4640b125c5dbac27b0b5bccd3fce16909819
Piedone <piedone@pyrocenter.hu>
2013-11-05 12:10:28 +0100

    Adding documentation

--- QuickParts/IQuickPartLogic.cs
+++ QuickParts/IQuickPartLogic.cs
@@ -3,13 +3,26 @@ using Orchard;
 
 namespace Lombiq.Abstractions.QuickParts
 {
+    /// <summary>
+    /// Describes a background logic for quick parts.
+    /// </summary>
     public interface IQuickPartLogic : IDependency
     {
     }
 
-    public interface IQuickPartLogic<T> : IQuickPartLogic
-        where T : IQuickPart
+    /// <summary>
+    /// Describes a background logic for a specific quick part type.
+    /// </summary>
+    /// <typeparam name="TPart">The type of the quick part</typeparam>
+    public interface IQuickPartLogic<TPart> : IQuickPartLogic
+        where TPart : IQuickPart
     {
-        IEnumerable<KeyValuePair<string, object>> ComputeDisplayParameters(T part);
+        /// <summary>
+        /// When the part is displayed, this method is called on every corresponding logic to compute additional parameters for
+        /// be used in the display template.
+        /// </summary>
+        /// <param name="part">The part object</param>
+        /// <returns>The collection of parameters that will be reachable from the template</returns>
+        IEnumerable<KeyValuePair<string, object>> ComputeDisplayParameters(TPart part);
     }
 }
