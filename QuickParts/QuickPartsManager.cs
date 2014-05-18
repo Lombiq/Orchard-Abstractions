bbf54e15365ba89062fdd9f9c9d50b0181bd3f26
Benedek Farkas <bfarkas@windowslive.com>
2014-05-18 15:45:08 +0200

    Fixing Castle references according to Orchard update

--- QuickParts/QuickPartsManager.cs
+++ QuickParts/QuickPartsManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Castle.Core.Interceptor;
 using Castle.DynamicProxy;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.FieldStorage.InfosetStorage;
